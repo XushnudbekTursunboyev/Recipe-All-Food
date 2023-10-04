8e9d2a3502f6eb69b49df81bf5e21a52f5a7adae
Xushnudbek <86800818+XushnudbekTursunboyev@users.noreply.github.com>
2023-10-04 18:37:07 +0500

    ui update

--- app/src/main/java/com/example/recipe/utils/CONSTANTS.kt
+++
@@ -1,6 +0,0 @@
-package com.example.recipe.utils
-
-object CONSTANTS {
-    const val READ_STORAGE_PERM = 123
-    const val MEAL_ITEM = "item"
-}
--- app/src/main/java/com/example/recipe/utils/extensions/Extensions.kt
+++
@@ -1,4 +0,0 @@
-package com.example.recipe.utils.extensions
-
-class Extensions {
-}
